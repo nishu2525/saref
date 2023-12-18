2b93a0814fff4779c42a48538e88f50efd8fc030
Sare@20 <sarefoundation@gmail.com>
2023-12-18 12:14:29 +0530

    Sarefile

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import { Home } from './Components/Home'
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Nav from './Header/Nav';
+// import navbar from './Header/Navbar'
+import Footer from './Components/Footer/Footer';
+import Donate from './Header/Donate';
+import JoinPage from './Components/JoinPage';
+import Reports from './Components/Reports';
+import Children from './Components/Children';
+import Donatebtn from './Components/Donatebtn';
+import Joinus from './Components/Joinus';
+import Women from './Components/Women';
+import Lgbtqi from'./Components/Lgbtqi';
+import TermsPg from'./Components/TermsPg';
+import RefundPg from './Components/RefundPg';
+import Csr from './Components/Csr';
+
 
 function App() {
   return (
+   
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Nav />
+     <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/about" element={<About/>}></Route>
+      <Route path="/contact" element={<Contact/>}></Route>
+      <Route path='/donate' element={<Donate/>}></Route>
+      <Route path='/joinpage' element={<JoinPage/>}></Route>
+      <Route path='/reports' element={<Reports/>}></Route>
+      <Route path='/Children' element={<Children/>}></Route>
+      <Route path='/women' element={<Women/>}></Route>
+      <Route path='/lgbtqi' element={<Lgbtqi/>}></Route>
+      <Route path='/Csr' element={<Csr/>}></Route>
+      <Route path='/terms_and_conditions' element={<TermsPg/>}></Route>
+      <Route path='/refund_policy' element={<RefundPg/>}></Route>
+     </Routes>
+     <Donatebtn/>
+      <Joinus />
+     <Footer/>  
     </div>
+    
+
   );
 }
 
